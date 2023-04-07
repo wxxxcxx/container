8f9a618dccdb6d10670547f1fb5b2adcb776aff6
Wx <wxxxcxx@gmail.com>
2023-04-08 00:29:51 +0800

    Update xray

--- xray/start.sh
+++ xray/start.sh
@@ -1,16 +1,16 @@
-#! /bin/bash
+#!/usr/bin/dumb-init /bin/bash
 set -e
-function setup() {
-    if [ $LICENSE ]; then
-        warp-cli --accept-tos set-license $LICENSE
-    else
-        warp-cli --accept-tos register
-    fi
-    warp-cli --accept-tos set-mode proxy
-    warp-cli --accept-tos connect
-}
-
-warp-svc > warp.log 2 > &1 &
+warp-svc 2>&1 >warp.log &
 sleep 2
-setup
+echo "Initializing warp license..."
+if [ $LICENSE ]; then
+    warp-cli --accept-tos set-license $LICENSE
+else
+    warp-cli --accept-tos register
+fi
+echo "Changing warp mode..."
+warp-cli --accept-tos set-mode proxy
+echo "Connecting to warp..."
+warp-cli --accept-tos connect
+echo "Starting xray..."
 /xray/xray run -config /xray/config.json
