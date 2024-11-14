83bb446e2c04e5914a6f30b447ace504817e8ba5
W <wxxxcxx@foxmail.com>
2024-11-14 18:08:50 +0800

    feat: 使用 Cloudflare WARP 构建镜像
    更新镜像构建过程，使用 Cloudflare WARP 构建镜像，并修复健康检查脚本以确保与 WARP 客户端兼容。
    
    - 镜像构建过程已优化，改用 Cloudflare WARP 构建镜像，并修复健康检查脚本，确保与 WARP 客户端兼容。
    + 此变更引入了以下修改：
    +
    + - 使用 `debian:bookworm` 作为基础镜像。
    + - 安装 Cloudflare WARP 客户端。
    + - 添加 `gost` 工具以提供代理功能。
    + - 修改健康检查脚本以修复与 WARP 客户端的兼容性问题。
    + - 修改容器入口脚本以启动 WARP 客户端和代理服务。
    +
    + 此变更旨在提供更稳定、更安全的容器镜像，同时确保与 Cloudflare WARP 的最佳兼容性。

---
+++ warp/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# create a tun device if not exist to ensure compatibility with Podman
+if [ ! -e /dev/net/tun ]; then
+    mkdir -p /dev/net
+    mknod /dev/net/tun c 10 200
+    chmod 600 /dev/net/tun
+fi
+
+# start dbus
+mkdir -p /run/dbus
+if [ -f /run/dbus/pid ]; then
+    rm /run/dbus/pid
+fi
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf
+
+# start the daemon
+warp-svc --accept-tos &
+
+# sleep to wait for the daemon to start, default 2 seconds
+sleep "$WARP_SLEEP"
+
+# if /var/lib/cloudflare-warp/reg.json not exists, setup new warp client
+if [ ! -f /var/lib/cloudflare-warp/reg.json ]; then
+    # if /var/lib/cloudflare-warp/mdm.xml not exists or REGISTER_WHEN_MDM_EXISTS not empty, register the warp client
+    if [ ! -f /var/lib/cloudflare-warp/mdm.xml ] || [ -n "$REGISTER_WHEN_MDM_EXISTS" ]; then
+        warp-cli registration new && echo "Warp client registered!"
+        # if a license key is provided, register the license
+        if [ -n "$WARP_LICENSE_KEY" ]; then
+            echo "License key found, registering license..."
+            warp-cli registration license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
+        fi
+    fi
+    # connect to the warp server
+    warp-cli --accept-tos connect
+else
+    echo "Warp client already registered, skip registration"
+fi
+
+# start the proxy
+gost -L :"$PORT"
